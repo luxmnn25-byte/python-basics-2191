de5dca77b2341e87f5be952df460d511059080ae
luxmnn25-byte <luxmnn25@gmail.com>
2026-09-18 22:36:32 +0530

    Add Fibonacci and student class programs

---
+++ struct.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+student = Student("Rahul", 12)
+print("Name:", student.name)
+print("Roll No:", student.roll_no)
